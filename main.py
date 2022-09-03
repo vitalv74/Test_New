882d9edf0527e195454e9c553f6c945005969d8f
Nastya <vitalv74@mail.ru>
2022-09-03 14:34:13 +0400

    fgertrytruytyuytiuyiuyiuyi

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ fghghfgh
 
 # Press Shift+F10 to execute it or tyuiyiuyiuyiouyiuyiyuiyuiyreplace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
+yutyu
+rtutyuty
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name} рuyrtuопgjhрvcbcvb')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
-if __kjlkljkljkname__ == '__main__':
+if __kjlkljkljkname__ == '__makjhkjhkjhkjhin__':
     print_hi('PyCharm отtyutyпghjhроп')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
