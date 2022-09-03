d3a5506263a6a99ae8edea3d07312b9b3c4d439f
vitalv74 <112749010+vitalv74@users.noreply.github.com>
2022-09-03 14:34:45 +0400

    99999999999999999999676767676767

--- main.py
+++ main.py
@@ -8,7 +8,16 @@ rtutyuty
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name} рuyrtuопgjhрvcbcvb')  # Press Ctrl+F8 to toggle the breakpoint.
-
+ljkljkl
+jkl
+jkl
+jk
+l
+jkl
+k
+jl
+jk
+l
 
 # Press the green button in the gutter to run the script.
 if __kjlkljkljkname__ == '__makjhkjhkjhkjhin__':
